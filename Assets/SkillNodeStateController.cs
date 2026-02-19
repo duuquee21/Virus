adb8c57a77f085017256112e4fd8335c83fd5b5a
Mark <markel_quevedo@euneiz.com>
2026-02-19 11:16:06 +0100

    Pequeño cambio boton

--- Assets/SkillNodeStateController.cs
+++ Assets/SkillNodeStateController.cs
@@ -3,12 +3,8 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.EventSystems;
 
-
 public class SkillNodeStateController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
-
 {
-   
-
     public SkillNode skillNode;
     public Button button;
     public Image nodeImage;
@@ -28,6 +24,7 @@ public class SkillNodeStateController : MonoBehaviour, IPointerEnterHandler, IPo
 
         UpdateState();
     }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         isHovered = true;
@@ -40,34 +37,43 @@ public class SkillNodeStateController : MonoBehaviour, IPointerEnterHandler, IPo
 
     void UpdateState()
     {
-        if (isHovered && hoverSprite != null)
-        {
-            SetVisual(hoverSprite);
-            return;
-        }
-
+        // 1. Si ya está desbloqueado (prioridad máxima)
         if (skillNode.IsUnlocked)
         {
-            SetVisual(disabledSprite);
+            // Pasamos 'false' para que el botón no sea interactuable
+            SetVisual(disabledSprite, false);
             return;
         }
 
+        // 2. Si no hay suficiente dinero
         if (LevelManager.instance.contagionCoins < skillNode.CoinCost)
         {
-            SetVisual(notEnoughMoneySprite);
+            // Pasamos 'false' para que el botón no sea interactuable
+            SetVisual(notEnoughMoneySprite, false);
             return;
         }
 
-        SetVisual(normalSprite);
+        // 3. A partir de aquí, el nodo ES comprable. 
+        // Si el ratón está encima, mostramos el hover.
+        if (isHovered && hoverSprite != null)
+        {
+            // Pasamos 'true' porque se puede comprar
+            SetVisual(hoverSprite, true);
+            return;
+        }
+
+        // 4. Si se puede comprar pero el ratón NO está encima, estado normal.
+        SetVisual(normalSprite, true);
     }
 
-    void SetVisual(Sprite sprite)
+    // Añadimos el parámetro 'bool isInteractable' para controlar el botón
+    void SetVisual(Sprite sprite, bool isInteractable)
     {
         if (nodeImage != null && sprite != null)
             nodeImage.sprite = sprite;
 
         if (button != null)
-            button.interactable = true;
+            button.interactable = isInteractable; // Activamos o desactivamos el botón aquí
 
         if (costText != null)
             costText.text = skillNode.CoinCost.ToString();
