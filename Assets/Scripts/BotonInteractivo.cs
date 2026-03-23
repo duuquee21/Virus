d21eb32395f204456f3742079b365750a2a06a06
Mark <markel_quevedo@euneiz.com>
2026-03-23 12:06:12 +0100

    Cambio botones

---
+++ Assets/Scripts/BotonInteractivo.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+using System.Collections;
+
+public class BotonInteractivo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Image fondo;
+    private TextMeshProUGUI texto;
+    private Quaternion rotacionOriginal;
+    private Coroutine corrutinaActual;
+
+    [Header("Colores")]
+    public Color colorFondoHover = Color.black;
+    public Color colorTextoHover = Color.green;
+
+    [Header("Configuración del Balanceo")]
+    public float anguloShake = 5f;
+    public float velocidadGiro = 40f;
+    public int repeticiones = 1;
+
+    void Awake()
+    {
+        fondo = GetComponent<Image>();
+        texto = GetComponentInChildren<TextMeshProUGUI>();
+        rotacionOriginal = transform.localRotation;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // 1. Matar cualquier movimiento previo para evitar conflictos
+        ResetearEstado();
+
+        // 2. Aplicar colores de Hover
+        fondo.color = colorFondoHover;
+        texto.color = colorTextoHover;
+
+        // 3. Iniciar el shake
+        corrutinaActual = StartCoroutine(EfectoBalanceoSuave());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // 1. Detener todo inmediatamente
+        ResetearEstado();
+
+        // 2. Forzar vuelta a la normalidad estética
+        fondo.color = Color.white;
+        texto.color = Color.black;
+        transform.localRotation = rotacionOriginal;
+    }
+
+    private void ResetearEstado()
+    {
+        if (corrutinaActual != null)
+        {
+            StopCoroutine(corrutinaActual);
+            corrutinaActual = null;
+        }
+    }
+
+    IEnumerator EfectoBalanceoSuave()
+    {
+        for (int i = 0; i < repeticiones; i++)
+        {
+            yield return StartCoroutine(GirarA(anguloShake));
+            yield return StartCoroutine(GirarA(-anguloShake));
+        }
+        // Volver al centro suavemente al terminar el ciclo
+        yield return StartCoroutine(GirarA(0));
+    }
+
+    IEnumerator GirarA(float anguloTarget)
+    {
+        Quaternion destino = Quaternion.Euler(0, 0, anguloTarget);
+
+        // Usamos un contador de seguridad para que no se quede trabado en el while
+        float tiempoSeguridad = 0;
+        while (Quaternion.Angle(transform.localRotation, destino) > 0.01f && tiempoSeguridad < 0.5f)
+        {
+            transform.localRotation = Quaternion.Slerp(
+                transform.localRotation,
+                destino,
+                Time.deltaTime * velocidadGiro
+            );
+            tiempoSeguridad += Time.deltaTime;
+            yield return null;
+        }
+        transform.localRotation = destino;
+    }
+}
